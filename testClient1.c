f860b77e7199a1100f694c5703f51d51cda87cda
KUEE1080 <dongjae0517@naver.com>
2017-01-05 16:19:15 +0900

    hi

--- testClient1.c
+++ testClient1.c
@@ -22,16 +22,21 @@ int CharToInt(char chunk[], int index, int length)
 
 int main( int argc, char **argv)
 {
+
    while(1){
 
-      int   client_socket;
+      int client_socket;
+
       struct sockaddr_in   server_addr;
-      char   buff[BUFF_SIZE+5];
-      //char c_UserPosX[3], c_UserPosY[3], c_UserDesX[3], c_UserDesY[3], c_weight;//it is temp.. 
-      int User_PosX, User_PosY, User_DesX, User_DesY, weight, Car_PosX, car_PosY; 
-      //you should write a code that reads the txt file and input the value of the variable
+      char buff[BUFF_SIZE+5];
+      char Data_send[100];
 
       client_socket  = socket( PF_INET, SOCK_STREAM, 0);
+
+      int User_PosX, User_PosY, User_DesX, User_DesY, weight, Car_PosX, Car_PosY;
+      int calculated_weight = -1;//default value
+      calculated_weight = 80;
+      //you should write a code that reads the txt file and input the value of the variable
 
       if( -1 == client_socket)
       {
@@ -49,18 +54,36 @@ int main( int argc, char **argv)
          printf( "접속 실패\n");
          exit(1);
       }
+
+      char arr_t[20]; int k;
+      if(calculated_weight == -1)
+      {printf("다익스트라 알고리즘이 변수에 전달 안됨 /n");}
+      else
+      {
+         sprintf(arr_t,"%d", calculated_weight);
+         for(k = 0 ; k < strlen(arr_t) ; k++)
+         {
+            Data_send[k + 1] = arr_t[k];
+         }
+      }
+
+      Data_send[0] = 'R';
       //write( client_socket, argv[1], strlen( argv[1])+1);      // +1: NULL까지 포함해서 전송
-      write( client_socket, "1", strlen("1") + 1);//sending its car serial number car1 : 1, car2 : 2
+      //write( client_socket, "R", strlen("R") + 1);//sending its car serial number car1 : 1, car2 : 2
+      write( client_socket, Data_send, strlen(Data_send) + 1);
       read ( client_socket, buff, BUFF_SIZE);
 
-      User_PosX = CharToInt(buff,2,3);
-      User_PosY = CharToInt(buff,6,3);
-      User_DesX = CharToInt(buff,12,3);
-      User_DesY = CharToInt(buff,16,3);
-      weight = CharToInt(buff,22,2);
+      User_PosX = CharToInt(buff,0,3);
+      User_PosY = CharToInt(buff,3,3);
+      User_DesX = CharToInt(buff,6,3);
+      User_DesY = CharToInt(buff,9,3);
+      weight = CharToInt(buff,12,3);
+      Car_PosX = CharToInt(buff,15,3);
+      Car_PosY = CharToInt(buff,18,3);
 
+      //printf("%s\n", buff);
       printf( "%d \n", User_PosX); printf( "%d \n", User_PosY); printf( "%d \n", User_DesX); printf( "%d \n", User_DesY); printf( "%d \n", weight);
-      //sscanf(myarray, "%d", &i);
+      printf( "%d \n", Car_PosX); printf( "%d \n", Car_PosY);
       close(client_socket);  
       sleep(1);//1 second    
    }
--- testServer.c
+++ testServer.c
@@ -17,13 +17,14 @@ int main (void)
    struct sockaddr_in   server_addr;
    struct sockaddr_in   client_addr;
 
-   char buff_rcv[BUFF_SIZE+5];
+   char buff_rcv[BUFF_SIZE+5];//26,30
    char buff_snd[BUFF_SIZE+5];
-   char Data_send[23] = {'P','/','1','0','0','/','2','0','0','/','D','/','1','2','9','/','5','4','7','/','w','/','1'};
-
-   int User_Pos[2]; User_Pos[0] = 100; User_Pos[1] = 200;
-   int User_Des[2]; User_Des[0] = 129; User_Des[1] =547;
+   char Data_send[21] = {'1','0','0','2','0','0','1','2','9','5','4','7','0','0','1','6','1','1','3','1','1'};//sample data
+   //char User_Pos[2], User_Des[2], weight;
+   //order : P,user position x , user position y, user destination x, user destination y, weight
    int weight = 1;
+
+   //User_Pos[0] = 100; User_Pos[1] = 200; User_Des[0] = 129; User_Des[1] = 547;
 
    server_socket = socket( PF_INET, SOCK_STREAM, 0);
    if( -1 == server_socket)
@@ -49,7 +50,7 @@ int main (void)
       exit(1);
    }
 
-   while(1)
+   while(1)//infinite loop
    {
       client_addr_size = sizeof(client_addr);
       client_socket = accept(server_socket, (struct sockaddr*)&client_addr, &client_addr_size);
@@ -67,13 +68,8 @@ int main (void)
       //if server receives distance value and all the values are returning z value, weight++
       //if min value defined, send the activation data to the car which has the min value
       sprintf( buff_snd, "%d : %s", strlen( buff_rcv), buff_rcv);
-      //String temp;
-      //sprintf(temp, "%d", weight);
       //write( client_socket, buff_snd, strlen( buff_snd)+1);          // +1: NULL까지 포함해서 전송
-      //write( client_socket, "alpha", strlen("alpha")+1);
       write( client_socket, Data_send, strlen(Data_send)+1);
-      //write( client_socket, "D,129,547", strlen("D,129,547")+1);
-      //write( client_socket, "w,1", strlen("w,1")+1);
       close( client_socket);
    }
 }
