d3e3e16d0d42749aa04fd42208a61e716e6a5800
KUEE1080 <dongjae0517@naver.com>
2017-01-06 11:43:42 +0900

    hihi

--- testClient1.c
+++ testClient1.c
@@ -8,8 +8,17 @@
 
 #define  BUFF_SIZE   1024
 
-int CharToInt(char chunk[], int index, int length);
-
+int CharToInt(char chunk[], int index, int length)
+{
+   int result = 0; int i;
+   char arr[3];
+   for(i = 0 ; i < length ; i++)
+   {
+      arr[i] = chunk[i + index];
+   }
+   result = atoi(arr);
+   return result;
+}
 
 int main( int argc, char **argv)
 {
@@ -21,14 +30,14 @@ int main( int argc, char **argv)
       struct sockaddr_in   server_addr;
       char buff[BUFF_SIZE+5];
       char Data_send[100];
+      char selectedserial = 'x';
 
       client_socket  = socket( PF_INET, SOCK_STREAM, 0);
 
-      int User_PosX, User_PosY, User_DesX, User_DesY, weight, Car_PosX, Car_PosY, In_locX, In_locY;
+      int userloc[2], dest[2], weight, carloc[2], interruptloc[2], onboard;
       int calculated_best = -1;//default value
       //calculated_best = 80;
       //you should write a code that reads the txt file and input the value of the variable
-      //reply - no you don't need to
 
       if( -1 == client_socket)
       {
@@ -39,7 +48,7 @@ int main( int argc, char **argv)
       memset( &server_addr, 0, sizeof( server_addr));
       server_addr.sin_family = AF_INET;
       server_addr.sin_port = htons(4000);
-      server_addr.sin_addr.s_addr= inet_addr("192.168.0.16");//write the server ip address
+      server_addr.sin_addr.s_addr= inet_addr("192.168.0.21");//write the server ip address
 
       if( -1 == connect( client_socket, (struct sockaddr*)&server_addr, sizeof( server_addr) ) )
       {
@@ -49,7 +58,7 @@ int main( int argc, char **argv)
 
       char arr_t[20]; int k;
       if(calculated_best == -1)
-      {printf("다익스트라 알고리즘이 변수에 전달 안됨 /n");}
+      {printf("다익스트라 알고리즘이 변수에 전달 안됨 \n");}
       else
       {
          sprintf(arr_t,"%d", calculated_best);
@@ -65,33 +74,20 @@ int main( int argc, char **argv)
       write( client_socket, Data_send, strlen(Data_send) + 1);
       read ( client_socket, buff, BUFF_SIZE);
 
-      User_PosX = CharToInt(buff,0,3);
-      User_PosY = CharToInt(buff,3,3);
-      User_DesX = CharToInt(buff,6,3);
-      User_DesY = CharToInt(buff,9,3);
+      userloc[0] = CharToInt(buff,0,3);
+      userloc[1] = CharToInt(buff,3,3);
+      dest[0] = CharToInt(buff,6,3);
+      dest[1] = CharToInt(buff,9,3);
       weight = CharToInt(buff,12,3);
-      Car_PosX = CharToInt(buff,15,3);
-      Car_PosY = CharToInt(buff,18,3);
-      In_locX = CharToInt(buff,21,3);
-      In_locY CharToInt(buff,24,3);
+      carloc[0] = CharToInt(buff,15,3);
+      carloc[1] = CharToInt(buff,18,3);
+      interruptloc[0] = CharToInt(buff,21,3);
+      interruptloc[1]= CharToInt(buff,24,3);
       //printf("%s\n", buff);
-      printf( "%d \n", User_PosX); printf( "%d \n", User_PosY); printf( "%d \n", User_DesX); printf( "%d \n", User_DesY); printf( "%d \n", weight);
-      printf( "%d \n", Car_PosX); printf( "%d \n", Car_PosY);
+      printf( "%d \n", userloc[0]); printf( "%d \n", userloc[1]); printf( "%d \n", dest[0]); printf( "%d \n", dest[1]); printf( "%d \n", weight);
+      printf( "%d \n", carloc[0]); printf( "%d \n", carloc[1]); printf( "%d \n", interruptloc[0]); printf( "%d \n", interruptloc[1]);
       close(client_socket);  
       sleep(1);//1 second    
    }
    return 0;
-}
-
-
-int CharToInt(char chunk[], int index, int length)
-{
-   int result = 0; int i;
-   char arr[3];
-   for(i = 0 ; i < length ; i++)
-   {
-      arr[i] = chunk[i + index];
-   }
-   result = atoi(arr);
-   return result;
 }
